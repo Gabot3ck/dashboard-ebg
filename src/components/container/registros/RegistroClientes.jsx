f74a715828eb7c4cd2f5dde7e01865703194e5f8
Gabot3ck <jesus.castilloc@outlook.com>
2022-12-27 10:11:55 -0300

    se agregan ver los registros de los clientes

--- src/components/container/registros/RegistroClientes.jsx
+++ src/components/container/registros/RegistroClientes.jsx
@@ -1,17 +1,40 @@
-import {collection, doc, setDoc} from 'firebase/firestore';
+import {collection, doc, setDoc, onSnapshot, query, orderBy} from 'firebase/firestore';
 import db from '../../../backend/DBFiresbase';
 import FormRegistroClientes from '../../pure/form/FormRegistroClientes';
 import styles from './Registros.module.css';
+import {useState, useEffect} from 'react';
+import moment from 'moment';
 
 export default function RegistroClientes() {
 
     // Enviando datos a Firebase
     const setData = async (linkObjeto) => {
-
         const nuevoCliente = doc(collection(db, "clientes"));
         await setDoc(nuevoCliente, linkObjeto);
     }
 
+    // Obteniendo  datos de Firebase
+    const [clientes, setClientes] = useState([]);
+
+    const q = query(collection(db, "clientes"), orderBy("fechaRegistro", "desc"));
+
+    const getData = async () => {
+    onSnapshot(q, (querySnapshot) => {
+            const docs = [];
+    
+            querySnapshot.forEach((doc) => {
+                docs.push({...doc.data(), id:doc.id});
+            });
+            setClientes(docs);
+            // setTablaUsuarios(docs);
+        });
+    }
+
+    // Mostrando los datos en la interfaz
+    useEffect(() => {
+        getData();
+        
+    }, []);
 
     return (<>
         <div className={styles.WrapperContenedor} >
@@ -34,6 +57,45 @@ export default function RegistroClientes() {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            {/* Se muestran los proyectos registrados */}
+            <div className="table-responsive text-center mt-5" style={{overflowX: "scroll"}}>
+                <table id="tablaProyectos" className="table table-sm table-bordered text-center" style={{minWidth: "120%"}}> 
+                    <thead>
+                        <tr className="table-secondary" style={{fontSize: ".9rem"}} >
+                            <th>Razón Social</th>
+                            <th>Suministro</th>
+                            <th>Dirección</th>
+                            <th>Comuna</th>
+                            <th>Región</th>
+                            <th>Contacto</th>
+                            <th>Celular</th>
+                            <th>Teléfono</th>
+                            <th>Correo</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {clientes.map((el,index) => {
+                            return(
+                                <tr key= {index}  className="table-warning" style={{fontSize: ".85rem"}}>
+                                    <td>{el.razonSocial}</td>
+                                    <td>{el.suministro}</td>
+                                    <td>{el.ubicacion.direccion}</td>
+                                    <td>{el.ubicacion.comuna}</td>
+                                    <td>{el.ubicacion.region}</td>
+                                    <td>{el.contacto.nombreContacto} {el.contacto.apellidoContacto}</td>
+                                    <td >{el.contacto.cellContacto}</td>
+                                    <td>{el.contacto.fonoContacto}</td>
+                                    <td>{el.contacto.emailContacto}</td>
+                                </tr>
+                            )
+                        })}
+
+                    </tbody>
+
+                </table>
             </div>
 
         </div>
