811bd24093e9be1892edb43871c8be686c770c38
Gabot3ck <jesus.castilloc@outlook.com>
2023-01-06 13:26:51 -0300

    se condiconan los enlaces para ser vistos de acuerdo al perfil de usuario

--- src/components/pure/header/Header.jsx
+++ src/components/pure/header/Header.jsx
@@ -1,10 +1,11 @@
 import moment from "moment/moment";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import {useAuth} from '../../../context/authContext';
 
 
 export default function Header() {
-
+    const {user} = useAuth();
 
 
     return (<>
@@ -13,10 +14,15 @@ export default function Header() {
                 <Link to="/home" className="btn btn-danger" >Ir a Inicio</Link>
             </div>
             <div className="d-flex align-items-center justify-content-between w-75 ">
-                <Link to="/home/resultados/oocc" className="text-white" >OOCC</Link>
-                <Link to="/home/resultados/remodelaciones" className="text-white" >Remodelaciones</Link>
-                <Link to="/home/resultados/licitaciones" className="text-white" >Licitaciones</Link>
-                <Link to="/home/resultados/muebleria" className="text-white" >Mueblería</Link>
+                {user.perfil === "Gerente" && 
+                <div className="d-flex align-items-center justify-content-between w-75">
+                    <Link to="/home/resultados/oocc" className="text-white" >OOCC</Link>
+                    <Link to="/home/resultados/remodelaciones" className="text-white" >Remodelaciones</Link>
+                    <Link to="/home/resultados/licitaciones" className="text-white" >Licitaciones</Link>
+                    <Link to="/home/resultados/muebleria" className="text-white" >Mueblería</Link>
+                </div>
+                }
+
                 <p className="m-0">{moment().format("DD MMM YY")} </p>
             </div>
             
