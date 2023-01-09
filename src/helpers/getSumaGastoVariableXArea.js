7ebd6341227e42c54ca3b54a0cdf0edd5bceb6b6
Gabot3ck <jesus.castilloc@outlook.com>
2023-01-09 15:48:04 -0300

    se modifican los gastos x area

--- src/helpers/getSumaGastoVariableXArea.js
+++ src/helpers/getSumaGastoVariableXArea.js
@@ -28,7 +28,7 @@ const  getSumaGastoVariableXArea = (coleccion, clave, valor, estado) => {
             
         })
 
-        estado(lista.reduce((a,b) => a+b));
+        estado(lista.length ? lista.reduce((a,b) => a+b) : 0);
     });
 
 }
