b3a99dff95fc9034494d184bd5f3f46b05c1e737
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-02-17 15:32:39 -0300

    se modifican los decimales de las imposiciones

--- src/helpers/getDescuentos.js
+++ src/helpers/getDescuentos.js
@@ -6,16 +6,16 @@ export const getDescuentos = ( imponible, imposicion ) => {
 
     switch (imposicion) {
         case "salud":
-            return  descuento = (parseInt(imponible) * 0.07).toFixed(3);
+            return  descuento = (parseInt(imponible) * 0.07).toFixed(0);
         
         case "cesantia":
-            return descuento = (parseInt(imponible) *  0.03).toFixed(3);
+            return descuento = (parseInt(imponible) *  0.03).toFixed(0);
 
         case "sis":
-            return descuento = (parseInt(imponible) *  0.0154).toFixed(3);
+            return descuento = (parseInt(imponible) *  0.0154).toFixed(0);
 
         case "mutual":
-            return descuento = (parseInt(imponible) * 0.0671).toFixed(3);
+            return descuento = (parseInt(imponible) * 0.0671).toFixed(0);
         default:
             break;
     }
